b91c1add130be93f77275cd15bc8b0d2eac20da6
Juan Diego Bustamante <JuanBacon@users.noreply.github.com>
2020-04-30 02:12:02 -0500

    Initial commit

---
+++ PaginaWebCursos/Forms/Formulario web1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace PaginaWebCursos
+{
+    public partial class Formulario_web1 : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string ccon = "Server=localhost;UserID=root;Password=root;Database=proyecto";
+            using (var con = new MySqlConnection(ccon))
+            {
+                con.Open();
+                    using(var cmd = new MySqlCommand("SELECT * FROM PAISES",con))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        DropDownList1.DataSource = reader;
+                        DropDownList1.DataValueField = "id_pais";
+                        DropDownList1.DataTextField = "nombre_pais";
+                        DropDownList1.DataBind();
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ PaginaWebCursos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PaginaWebCursos.Global" Language="C#" %>
